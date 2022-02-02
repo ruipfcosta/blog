97e0f1d962e08015508eade31baabde01d4a753e
Rui Costa <rui.pfcosta@gmail.com>
2022-02-02 00:12:08 +0000

    Tidy up

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Site",
-//    platforms: [
-//        .macOS(.v10_15)
-//    ],
     products: [
         .executable(
             name: "SiteBuilder",
